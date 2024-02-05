bbdcefa358847bd28ae520331142622fbc7cb50d
anaqvi1225 <anaqvi1225@gmail.com>
2024-02-05 16:38:27 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,25 +1,24 @@
-variable "vpc_id" {
-    description = "VPC ID"
-    type  = string
-}
-
 variable "instance_type" {
   description = "Instance Type"
   type        = string
 }
 
 variable "ami" {
-    description = "AMI ID"
-    type = string
+  description = "AMI ID"
+  type        = string
 }
 
 variable "key_pair" {
-    description = "Key Pair"
-    type = string
+  description = "Key Pair"
+  type        = string
 }
 
 variable "subnet_id" {
-    description = "Subnet ID"
-    type        = string
+  description = "Subnet ID"
+  type        = string
 }
 
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
